1ba8ac28b9465ce5a7803060e65bf50150e8e5fc
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-06-01 12:45:05 +0900

    [Refactor] #3 - 코드 리뷰 반영

--- SwiftUIStudy/SwiftUIStudy/CalculatorApp/CalculatorFeature.swift
+++ SwiftUIStudy/SwiftUIStudy/CalculatorApp/CalculatorFeature.swift
@@ -5,8 +5,16 @@
 //  Created by 이명진 on 5/31/24.
 //
 
+//
+//  CalculatorFeature.swift
+//  SwiftUIStudy
+//
+//  Created by 이명진 on 5/31/24.
+//
+
 import ComposableArchitecture
 
+@Reducer
 struct CalculatorFeature: Reducer {
     
     @ObservableState
@@ -30,72 +38,100 @@ struct CalculatorFeature: Reducer {
         Reduce { state, action in
             switch action {
             case let .numberButtonTapped(button):
-                let number = button.buttonName
-                if state.inTheMiddleOfTyping {
-                    state.totalNumber += number
-                } else {
-                    state.totalNumber = number
-                    state.inTheMiddleOfTyping = true
-                }
-                return .none
+                return handleNumberButtonTapped(&state, button: button)
                 
             case let .operationButtonTapped(button):
-                state.previousNumber = state.totalNumber
-                state.operation = button
-                state.inTheMiddleOfTyping = false
-                return .none
+                return handleOperationButtonTapped(&state, button: button)
                 
             case .equalButtonTapped:
-                guard let operation = state.operation,
-                      let previousValue = Double(state.previousNumber),
-                      let currentValue = Double(state.totalNumber) else {
-                    return .none
-                }
-                
-                var result: Double = 0
-                
-                switch operation {
-                case .plus:
-                    result = previousValue + currentValue
-                case .minus:
-                    result = previousValue - currentValue
-                case .multiple:
-                    result = previousValue * currentValue
-                case .divide:
-                    result = previousValue / currentValue
-                default:
-                    break
-                }
-                
-                if result.truncatingRemainder(dividingBy: 1) == 0 {
-                    state.totalNumber = String(format: "%.0f", result)
-                } else {
-                    state.totalNumber = String(format: "%.6f", result)
-                }
-                
-                state.operation = nil
-                state.inTheMiddleOfTyping = false
-                return .none
+                return handleEqualButtonTapped(&state)
                 
             case .clearButtonTapped:
-                state.totalNumber = "0"
-                state.previousNumber = ""
-                state.operation = nil
-                state.inTheMiddleOfTyping = false
-                return .none
+                return handleClearButtonTapped(&state)
                 
             case .reverseButtonTapped:
-                if let value = Double(state.totalNumber) {
-                    state.totalNumber = String(value * -1)
-                }
-                return .none
+                return handleReverseButtonTapped(&state)
                 
             case .percentButtonTapped:
-                if let value = Double(state.totalNumber) {
-                    state.totalNumber = String(value / 100)
-                }
-                return .none
+                return handlePercentButtonTapped(&state)
             }
         }
     }
+}
+
+extension CalculatorFeature: CalculatorLogic {
+    func handleNumberButtonTapped(_ state: inout State, button: ButtonStyle) -> Effect<Action> {
+        let number = button.buttonName
+        if state.inTheMiddleOfTyping {
+            state.totalNumber += number
+        } else {
+            state.totalNumber = number
+            state.inTheMiddleOfTyping = true
+        }
+        return .none
+    }
+    
+    func handleOperationButtonTapped(_ state: inout State, button: ButtonStyle) -> Effect<Action> {
+        state.previousNumber = state.totalNumber
+        state.operation = button
+        state.inTheMiddleOfTyping = false
+        return .none
+    }
+    
+   func handleEqualButtonTapped(_ state: inout State) -> Effect<Action> {
+        guard let operation = state.operation,
+              let previousValue = Double(state.previousNumber),
+              let currentValue = Double(state.totalNumber) else {
+            return .none
+        }
+        
+        var result: Double = 0
+        
+        switch operation {
+        case .plus:
+            result = previousValue + currentValue
+        case .minus:
+            result = previousValue - currentValue
+        case .multiple:
+            result = previousValue * currentValue
+        case .divide:
+            result = previousValue / currentValue
+        default:
+            break
+        }
+        
+        if result.truncatingRemainder(dividingBy: 1) == 0 {
+            state.totalNumber = String(format: "%.0f", result)
+        } else {
+            state.totalNumber = String(format: "%.6f", result)
+        }
+        
+        state.operation = nil
+        state.inTheMiddleOfTyping = false
+        return .none
+    }
+    
+    func handleClearButtonTapped(_ state: inout State) -> Effect<Action> {
+        state.totalNumber = "0"
+        state.previousNumber = ""
+        state.operation = nil
+        state.inTheMiddleOfTyping = false
+        return .none
+    }
+    
+    func handleReverseButtonTapped(_ state: inout State) -> Effect<Action> {
+        if let value = Double(state.totalNumber) {
+            state.totalNumber = String(value * -1)
+        }
+        return .none
+    }
+    
+    func handlePercentButtonTapped(_ state: inout State) -> Effect<Action> {
+        if let value = Double(state.totalNumber) {
+            state.totalNumber = String(value / 100)
+        }
+        return .none
+    }
+    
+    
 }
